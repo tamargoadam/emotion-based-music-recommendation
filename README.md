179e928ae046b1e0f12ff9be4b3ae59afe5fa735
Jorge Castillo <jorgecastilloltsd@gmail.com>
2020-02-02 18:57:38 -0500

    Create a basic twitter endpoint and instantiate Pipfile config

---
+++ README.md
@@ -0,0 +1,17 @@
+# Testing the endpoints
+Clone and enter project repo
+```
+git clone https://github.com/tamargoadam/emotion-based-music-recommendation.git
+cd emotion-based-music-recommendation
+```
+install dependency manager and dependencies 
+(pipenv allows us to manage dependencies)
+```
+pip install --user pipenv
+pipenv install
+```
+pipenv should've installed the necessary libraries (pandas, tweepy, etc...). now use pipenv to create a virtualenv and run a script.
+```
+pipenv shell
+python3 twitter_popularsearch.py
+```
---
+++ twitter_popularsearch.py
@@ -0,0 +1,32 @@
+# Import the Twython class
+from twython import Twython
+import json
+import pandas as pd
+
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+# Create our query
+query = {'q': 'learn python',
+        'result_type': 'popular',
+        'count': 10,
+        'lang': 'en'
+        }
+
+
+# Search tweets
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in python_tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)
+df.sort_values(by='favorite_count', inplace=True, ascending=False)
+print(df.head(5))
