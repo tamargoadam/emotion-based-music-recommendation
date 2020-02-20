ba8a48869f9b08920aaa2448fc3a16c513457442
Adam Tamargo <tamargoadam@gmail.com>
2020-02-20 15:15:26 -0500

    add function to compile list of top and similar artists; add ability to specify amount of artists/tracks compiled by corresponding functions

--- server/endpoints/spotify_helpers.py
+++ server/endpoints/spotify_helpers.py
@@ -9,33 +9,57 @@ def authenticate_spotify(token):
     return sp
 
 
-def get_top_artists(sp: spotipy.Spotify):
-    """compiles list of user's top artists"""
+def get_top_artists(sp: spotipy.Spotify, amount: int = 20):
+    """compiles list of user's top artists of length amount"""
     print('...getting top artists')
-    top_artists_name = []
-    top_artists_uri = []
+    artists_name = []
+    artists_uri = []
     ranges = ['short_term', 'medium_term', 'long_term']
     for r in ranges:
-        all_top_artist_data = sp.current_user_top_artists(limit=50, time_range=r)
+        all_top_artist_data = sp.current_user_top_artists(limit=amount, time_range=r)
         top_artist_data = all_top_artist_data['items']
         for artist_data in top_artist_data:
-            if artist_data['name'] not in top_artists_name:
-                top_artists_name.append(artist_data['name'])
-                top_artists_uri.append(artist_data['uri'])
-    return top_artists_uri
+            if artist_data['name'] not in artists_name:
+                artists_name.append(artist_data['name'])
+                artists_uri.append(artist_data['uri'])
+    return artists_uri
 
 
-def get_artists_top_tracks(sp: spotipy.Spotify, top_artists_uri):
-    """compiles unordered list of top tracks made by artists in top_artists_uri"""
+def get_top_and_similar_artists(sp: spotipy.Spotify, amount: int = 20):
+    """compiles a list of top and similar artists of length amount"""
+    print('...getting top and similar artists')
+    artists_name = []
+    artists_uri = []
+    ranges = ['short_term', 'medium_term', 'long_term']
+    for r in ranges:
+        all_top_artist_data = sp.current_user_top_artists(limit=amount, time_range=r)
+        top_artist_data = all_top_artist_data['items']
+        for artist_data in top_artist_data:
+            if artist_data['name'] not in artists_name and len(artists_uri) < amount:
+                artists_name.append(artist_data['name'])
+                artists_uri.append(artist_data['uri'])
+                similar_artists_data = sp.artist_related_artists(artist_id=artist_data['uri'])
+                for index, similar_artist_data in enumerate(similar_artists_data['artists']):
+                    if similar_artist_data['name'] not in artists_name and len(artists_uri) < amount:
+                        artists_name.append(similar_artist_data['name'])
+                        artists_uri.append(similar_artist_data['uri'])
+                    if index == 2:
+                        break
+    return artists_uri
+
+
+def get_artists_top_tracks(sp: spotipy.Spotify, artists_uri, amount: int = 50):
+    """compiles unordered list of top tracks made by artists in artists_uri of length amount"""
     print('...getting top tracks for each artist')
-    top_tracks_uri = []
-    for artist in top_artists_uri:
+    tracks_uri = []
+    for artist in artists_uri:
         all_top_tracks_data = sp.artist_top_tracks(artist)
         top_tracks_data = all_top_tracks_data['tracks']
         for track_data in top_tracks_data:
-            top_tracks_uri.append(track_data['uri'])
-    random.shuffle(top_tracks_uri)
-    return top_tracks_uri
+            tracks_uri.append(track_data['uri'])
+    random.shuffle(tracks_uri)
+    tracks_uri = tracks_uri[0:amount]
+    return tracks_uri
 
 
 def get_emo_tracks(sp: spotipy.Spotify, top_tracks_uri, emotion):
--- server/endpoints/spotify_search.py
+++ server/endpoints/spotify_search.py
@@ -11,11 +11,11 @@ with open("spotify_credentials.json", "r") as file:
 token = spotipy.util.prompt_for_user_token(username, scope, creds['CLIENT_ID'], creds['CLIENT_SECRET'], redirect_url)
 
 if token:
-    print("made it")
     sp = authenticate_spotify(token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name'])
+    results = get_artists_top_tracks(sp, get_top_and_similar_artists(sp))
+    print('\nTOP TRACKS\n')
+    for uri in results:
+        track = sp.track(uri)
+        print(track['name'])
 else:
     print("Can't get token for ", username)
