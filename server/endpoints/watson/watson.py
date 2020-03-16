8c3afce8d2ddd535af21063fb7ab6eb32632029e
timdoozy <thinds96@ufl.edu>
2020-03-16 19:02:06 -0400

    run it to see test output

--- server/endpoints/watson/watson.py
+++ server/endpoints/watson/watson.py
@@ -30,29 +30,28 @@ with open("../credentials/watson_credentials.json", "r") as file:
 #Start of Methods
 def get_sentiment(screen_name):
     # Input: '../twitter_data/%s_tweets.csv'
-    data = pd.read_csv(tpath + '%s_tweets.csv' % screen_name, usecols=['text'], decimal=",", index_col=False, nrows=50)
-    print(data)
-    json_text_data = ""
-    for index, sentiment in data['text'].iteritems():
-        json_text_data = json_text_data + sentiment + ". "
+    #data = pd.read_csv(tpath + '%s_tweets.csv' % screen_name, usecols=['text'], decimal=",", index_col=False, nrows=50)
+    #print(data)
+    #json_text_data = ""
+    #for index, sentiment in data['text'].iteritems():
+        #json_text_data = json_text_data + sentiment + ". "
         #output = tone_analyzer.tone(sentiment, content_type='text/plain')
-    print(json_text_data)
-    x = { "text" : json_text_data }
-    y = json.dumps(x)
-    with open(wpath + '%s_sentiment.json' % screen_name, 'w') as outfile:
-        json.dump(y, outfile)
-
-    """
-    with open(join(os.getcwd(),
-               'data/.json')) as tone_json:
-    tone = tone_analyzer.tone(
-        tone_input=json.load(tone_json)['text'],
-        content_type='text/plain',
-        sentences=True).get_result()
-    print(json.dumps(tone, indent=2))
-
-    data.to_csv(wpath + '%s_sentiment.csv')
-    """
+    #print(json_text_data)
+    #x = { "text" : json_text_data }
+    #y = json.dumps(x)
+    #with open(wpath + '%s_sentiment.json' % screen_name, 'w') as outfile:
+        #json.dump(y, outfile)
 
     
+    with open(join(os.getcwd(), 'data/test.json')) as tone_json:
+        tone = tone_analyzer.tone(
+            tone_input=json.load(tone_json)['text'],
+            content_type='text/plain',
+            sentences=True).get_result()
+
+    print(json.dumps(tone, indent=2))
+
+    #data.to_csv(wpath + '%s_sentiment.csv')
+
+
 get_sentiment('timdoozy')
