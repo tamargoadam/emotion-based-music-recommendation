26ddbb30245927873e9eab945361f61a361ad66c
timdoozy <thinds96@ufl.edu>
2020-03-16 19:37:08 -0400

    working for timdoozy

--- server/endpoints/watson/watson.py
+++ server/endpoints/watson/watson.py
@@ -29,21 +29,22 @@ with open("../credentials/watson_credentials.json", "r") as file:
 
 #Start of Methods
 def get_sentiment(screen_name):
-    # Input: '../twitter_data/%s_tweets.csv'
-    #data = pd.read_csv(tpath + '%s_tweets.csv' % screen_name, usecols=['text'], decimal=",", index_col=False, nrows=50)
-    #print(data)
-    #json_text_data = ""
-    #for index, sentiment in data['text'].iteritems():
-        #json_text_data = json_text_data + sentiment + ". "
-        #output = tone_analyzer.tone(sentiment, content_type='text/plain')
-    #print(json_text_data)
-    #x = { "text" : json_text_data }
-    #y = json.dumps(x)
-    #with open(wpath + '%s_sentiment.json' % screen_name, 'w') as outfile:
-        #json.dump(y, outfile)
+    # Input: '../twitter/data/%s_tweets.csv'
+    data = pd.read_csv(tpath + '%s_tweets.csv' % screen_name, usecols=['text'], decimal=",", index_col=False, nrows=50)
+    json_text_data = ""
+    for index, sentiment in data['text'].iteritems():
+        json_text_data = json_text_data + sentiment + ". "
+    print(json_text_data)
+    with open(wpath + '%s_sentiment.json' % screen_name, 'w') as f:
+        json.dump(json_text_data, f)
 
-    
-    with open(join(os.getcwd(), 'data/test.json')) as tone_json:
+    x = { "text" : json_text_data }
+    with open(wpath + '%s_sentiment.json' % screen_name, 'w') as outfile:
+        json.dump(x, outfile)
+
+
+
+    with open(join(os.getcwd(), wpath + '%s_sentiment.json' % screen_name)) as tone_json:
         tone = tone_analyzer.tone(
             tone_input=json.load(tone_json)['text'],
             content_type='text/plain',
@@ -52,6 +53,6 @@ def get_sentiment(screen_name):
     print(json.dumps(tone, indent=2))
 
     #data.to_csv(wpath + '%s_sentiment.csv')
-
+    
 
 get_sentiment('timdoozy')
