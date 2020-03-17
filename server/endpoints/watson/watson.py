2563535f7fb606b1ad09d44d854608bd153fc654
timdoozy <thinds96@ufl.edu>
2020-03-17 11:24:21 -0400

    get_sentiment() method working
    sends a collection of tweets to watson api, saves results in a json file

--- server/endpoints/watson/watson.py
+++ server/endpoints/watson/watson.py
@@ -4,16 +4,7 @@ from os.path import join
 from ibm_watson import ToneAnalyzerV3
 from ibm_watson.tone_analyzer_v3 import ToneInput
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
 import pandas as pd 
-from datetime import date
-from datetime import datetime
-import time
-
-import preprocessor as p
-
-import re
-from textblob import TextBlob
 
 wpath = 'data/'
 tpath = '../twitter/data/'
@@ -27,9 +18,11 @@ with open("../credentials/watson_credentials.json", "r") as file:
         authenticator=authenticator)
         tone_analyzer.set_service_url(creds['URL'])
 
-#Start of Methods
+
+# 
 def get_sentiment(screen_name):
     # Input: '../twitter/data/%s_tweets.csv'
+    # Output: '../watson/data/%s_sentiment.json'
     data = pd.read_csv(tpath + '%s_tweets.csv' % screen_name, usecols=['text'], decimal=",", index_col=False, nrows=50)
     json_text_data = ""
     for index, sentiment in data['text'].iteritems():
@@ -43,14 +36,21 @@ def get_sentiment(screen_name):
         json.dump(x, outfile)
 
 
-
+    # tone analyzer api call from sentiment.json data
     with open(join(os.getcwd(), wpath + '%s_sentiment.json' % screen_name)) as tone_json:
         tone = tone_analyzer.tone(
             tone_input=json.load(tone_json)['text'],
             content_type='text/plain',
             sentences=True).get_result()
+    #print(json.dumps(tone, indent=2))
 
-    print(json.dumps(tone, indent=2))
+
+    # save results as sentiment data file for user
+    hello = json.dumps(tone)
+    output = json.loads(hello)
+    with open(wpath + '%s_sentiment.json' % screen_name, 'w', encoding='utf-8') as f:
+        json.dump(output, f, ensure_ascii=False, indent=2)
+
 
     #data.to_csv(wpath + '%s_sentiment.csv')
     
