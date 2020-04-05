51dddf826042187afbad3bfcf985b6451e3c77ed
Adam Tamargo <tamargoadam@gmail.com>
2020-04-05 16:53:25 -0400

    added text input to twitter card

--- client/src/components/twitter-card/index.js
+++ client/src/components/twitter-card/index.js
@@ -1,8 +1,9 @@
-import { h, Component } from 'preact';
+import { Component } from 'preact';
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import style from './style';
+import {TextField} from "preact-material-components";
 
 const VALIDATE_PATH = 'http://127.0.0.1:5000/twitter-username?user='
 
@@ -11,34 +12,49 @@ export default class TwitterCard extends Component {
         super(props);
 
         this.state = {
+            text_input: "",
             twitter_username: "",
             valid: false
         };
     }
 
     toggleButtonState = () => {
-        fetch(VALIDATE_PATH + 'atamargo')
+        let currentComponent = this;
+        fetch(VALIDATE_PATH + this.state.text_input)
             .then(function(response) {
                 return response.text();
             })
             .then( data => {
-                this.setState({
+                currentComponent.setState({
+                    ...state,
                     twitter_username: data,
                     valid: true
                 })
+            })
+            .catch(function(error) {
+                console.log(error);
             });
     }
 
     render() {
       return (
           <Card>
-            <div class={style.cardHeader}>
+              <div class={style.cardHeader}>
               <h2 class=" mdc-typography--title">Link your Twitter account</h2>
-            </div>
-            <Card.Actions onClick= {this.toggleButtonState}
-            >
-              <Card.ActionButton>SIGN IN</Card.ActionButton>
-            </Card.Actions>
+              </div>
+              <div>
+                  <TextField
+                  label="Username"
+                  onKeyUp={e => {
+                      this.setState({
+                          text_input: e.target.value
+                      });
+                  }}
+                  />
+              </div>
+              <Card.Actions onClick= {this.toggleButtonState}>
+                  <Card.ActionButton>SIGN IN</Card.ActionButton>
+              </Card.Actions>
           </Card>
     );
   }
