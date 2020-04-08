4bf9f908eeb772227b1c0744fb8f1d4cd48fa9bb
timdoozy <thinds96@ufl.edu>
2020-04-08 18:40:48 -0400

    ready to manipulate the 4 main tone scores

--- server/rs/rs.py
+++ server/rs/rs.py
@@ -14,6 +14,7 @@ import pandas as pd
 import random
 from __future__ import division, print_function, unicode_literals
 import os
+import math
 # Ignore useless warnings (see SciPy issue #5998)
 import warnings
 warnings.filterwarnings(action="ignore", message="^internal gelsd")
@@ -26,17 +27,18 @@ style.use("ggplot")
 
 from sklearn.metrics import silhouette_score
 
+def music_recommendation(username: str, num_songs: int) -> list:
+    sentiment = get_tones(username)  #get users sentiment tones from tweets
+    tone_songs = adjust_songs(sentiment, num_songs) #weigh the tones based on the number of songs supposed to be in the playlist
+    feature_data = get_spotify_playlist_data(username)  #get feature data from user spotify library of songs
+    
 
-def get_sentiment(username: str) -> list:
+
+
+def get_sentiment(username: str) -> dict:
     """generate list of sentiments corresponding to user's tweets"""
     sentiment_data = watson.get_sentiment(twitter.get_tweets_by_user(username))
-    # TODO: fix credential path issue
-
-    tones = []
-    for t in sentiment_data['document_tone']['tones']:
-        tone_score = dict(tone=t['tone_name'], score=t['score'])
-        print(tone_score)
-        tones.append(tone_score)
+    tones = watson.format_sentiment(sentiment_data)
     return tones
 
 def get_spotify_playlist_data(username: str) -> list:
@@ -58,28 +60,112 @@ def check_key(dict, key) -> bool:
     else
         return False
 
-def sentiment_recommendation(username: str) -> list: 
-    tones = get_sentiment(username)
-    music = get_spotify_playlist_data(username)
-    #songs = pd.DataFrame.from_dict(trackList)
-    tones = pd.DataFrame.from_dict(tones)
-    #tones: anger, fear, joy, sadness, analytical, confident, tentative
-    anger, fear, joy, sadness, analytical, confident, tentative = 0 
-    if check_key(tones, 'anger'):
-        anger = True 
-    if check_key(tones, 'fear'):
-        fear = True 
-    if check_key(tones, 'joy'):
-        joy = True
-    if check_key(tones, 'sadness'):
-        sadness = True 
-    if check_key(tones, 'analytical'):
-        analytical = True 
-    if check_key(tones, 'confident'):
-        confident = True 
-    if check_key(tones, 'tentative'):
-        tentative = True 
+def adjust_songs(tone_in: dict, song_num: int) -> dict:
+    #Returns a new dictionary containing the amount of songs of each tone to produce in the playlist
+    temp_calm = 0
+    temp_joy = 0
+    temp_anger = 0
+    temp_sad = 0
+    length = len(tone_in)
+    print(length)
+    print(tone_in)
+    print(tone_in['joy'])
+    count = 0
+    if 'joy' in tone_in.keys():
+        temp_joy = tone_in['joy']
+        count += 1
+    if 'anger' in tone_in.keys():
+        temp_anger = tone_in['anger']
+        count += 1
+    if 'sadness' in tone_in.keys():
+        temp_sad = tone_in['sadness']
+        count += 1
+    if 'calm' in tone_in.keys():
+        temp_calm = tone_in['calm']
+        count += 1
+    
+    div = 1 / count
+    sum1 = 0
+    
+    list_vals = [0, 0, 0, 0]
+    #joy, anger, sad, calm
+    
+    #getsum1
+    if temp_joy > 0:
+        temp_joy *= song_num
+        temp_joy *= div
+        temp_joy = math.floor(temp_joy)
+        list_vals[0] = temp_joy
+        sum1 += temp_joy
+    if temp_anger > 0:
+        temp_anger *= song_num
+        temp_anger *= div
+        temp_anger = math.floor(temp_anger)
+        list_vals[1] = temp_anger
+        sum1 += temp_anger
+    if temp_sad > 0:
+        temp_sad *= song_num
+        temp_sad *= div
+        temp_sad = math.floor(temp_sad)
+        list_vals[2] = temp_sad
+        sum1 += temp_sad
+    if temp_calm > 0:
+        temp_calm *= song_num
+        temp_calm *= div
+        temp_calm = math.floor(temp_calm)
+        list_vals[3] = temp_calm
+        sum1 += temp_calm
+    
+    #show_vals(temp_sad, temp_joy, temp_anger, temp_calm)
+    
+    #showing vals for testing
+    #for i in list_vals:
+        #print(i)
+        
+    #sum1 vs song_num
+    if sum1 < song_num:
+        while sum1 < song_num:
+            for i in range(len(list_vals)):
+                if(list_vals[i] == 0):
+                    i += 1
+                else:
+                    list_vals[i] = list_vals[i] + 1
+                    sum1 += 1
+    #highly unlikely sum1 > song_num
+    if sum1 > song_num:
+        print("ERROR: Exception, account for this case")
+    
+    #showing vals for testing    
+    #for i in list_vals:
+        #print(i)
+        
+    #list_val[joy, anger, sad, calm]
+    ret_dic = tone_in.copy()
+      
+    #update the output dictionary with new values
+    if 'joy' in ret_dic.keys():
+        ret_dic['joy'] = list_vals[0]
+    if 'anger' in ret_dic.keys():
+        ret_dic['anger'] = list_vals[1]
+    if 'sadness' in ret_dic.keys():
+        ret_dic['sadness'] = list_vals[2]
+    if 'calm' in ret_dic.keys():
+        ret_dic['calm'] = list_vals[3]
+        
+    return ret_dic
+    
+    #function for testing
+def show_vals(sad, joy, anger, calm):
+    print("Sad: %d" %sad)
+    print("Joy: %d" %joy)
+    print("Anger: %d" %anger)
+    print("Calm: %d" %calm)
+
+##
+##
 ## Machine Learning Functions Below -- not entirely functional yet
+##
+##
 def normalize_data(username: str) -> list:
     #preprocessing
     songs = get_spotify_playlist_data(username)
