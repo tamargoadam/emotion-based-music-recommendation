9ffa9eab9c634b6d85adf489e379ad1de72795c6
Timothy Hinds <butterstim@yahoo.com>
2020-08-12 17:13:20 -0400

    condense into one get_tweets() function

--- server/api/endpoints/twitter/twitter.py
+++ server/api/endpoints/twitter/twitter.py
@@ -20,22 +20,23 @@ def validate_user_exists(username: str) -> bool:
 
 
 def remove_url(txt: str) -> str:
-    """remove urls from tweets"""
+    """Helper function for get_tweets() used to remove any urls or special symbols found in tweets."""
     return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
 
 
-def get_tweets_by_user(username: str, num_tweets: int = 1) -> list:
-    """get user's 'num_tweets' most recent tweets"""
-    api = get_twitter_api()
-    tweets_data = api.user_timeline(screen_name=username)
-    tweets = [tweet_data.text for tweet_data in tweets_data]
-    return tweets[:num_tweets]
-
-
-# 200 is maximum amount of tweets at a time
-def get_all_tweets(username: str) -> list:
+def get_tweets(screen_name: str) -> list:
+    """Retrieves a list of the most recent 200 tweets for a given user. Links will be removed from tweets.
+    Returns an empty list if there are no tweets found for the given user."""
     api = get_twitter_api() 
-    alltweets = api.user_timeline(screen_name = username, count=200)
-    outtweets = [remove_url(tweet.text) for tweet in alltweets]
-    return outtweets
+    try:
+        alltweets = api.user_timeline(screen_name=screen_name, count=200)
+        outtweets = [remove_url(tweet.text) for tweet in alltweets]
+        if len(outtweets) == 0:
+            raise NoTweetsFound
+        return outtweets
+    except NoTweetsFound:
+        print(f'No tweets were found for the twitter account of {screen_name}.')
 
+
+class NoTweetsFound(Exception):
+    pass
